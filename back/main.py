f0b26121995df1358fa370e62f96c75f1351e32b
Alberto Polanco <prmus10@gmail.com>
2020-08-24 12:58:19 -0500

    Syntax errors were fixed

--- back/main.py
+++ back/main.py
@@ -1,22 +1,32 @@
 from flask import Flask
 from flask_cors import CORS
-import myNeo4j
+from apineo4j import apineo4j
 
 app = Flask(__name__)
+PORT = 5000
+DEBUG = False
 CORS(app)
 
-@app.route("/")
-def hello():
-    return "<h1>Ingresa la palabra a buscar en la barra de direcciones</h1>"
 
-@app.route("/<word>")
+@app.errorhandler(404)
+def not_found(error):
+    return "<h1>Not Found</h1>"
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "<h1>Ingresa la palabra a buscar en la barra de direcciones</h1><p>https://graphora.herokuapp.com/'Palabra'</p>"
+
+
+@app.route("/<word>", methods=['GET'])
 def getJSON(word):
-    data = myNeo4j.getData(word)
-    #data = word
-    if (data != "[]"):
+    data = apineo4j.getData(word)
+    
+    if data != "[]":
         return data
     else:
         return "<h1>Palabra invalida</h1>"
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=PORT, debug=DEBUG)
