010cb7984a829bffe415dc6f07d01822a6d71951
Alberto <prmus10@gmail.com>
2020-08-31 20:09:54 -0500

    New function was added to get table information and generateJSON function was modified

--- back/apineo4j/apineo4j.py
+++ back/apineo4j/apineo4j.py
@@ -4,15 +4,8 @@ import json
 import os
 
 
-# things to use: get, _id, _labels (list), _properties
-# {
-#   name: name of the node,
-#   status: PalabrasAsociadas/PalabraEstimulo,
-#   direction: In/Out,
-#   other_nodes: {node_1, node_2}
-# }
-
 # Functions
+#   Graph
 def getData(word):
     # Create the driver
     db_url = os.environ.get("GRAPHENEDB_BOLT_URL")
@@ -31,20 +24,38 @@ def getData(word):
 
 
 def generateJSON(session, result):
+    # {
+    #   name: name of the node,
+    #   status: PalabrasAsociadas/PalabraEstimulo,
+    #   direction: In/Out,
+    #   other_nodes: {node_1, node_2}
+    # }
+    # .
+    # .
+    # .
     Argus = []
     for records in result:
+        flag = False
         name = records["n"]._properties["name"]
         status = list(records["n"]._labels)[0]
 
-        if records["r"].nodes[0]._properties["name"] == records["p"]._properties["name"]:
-            direction = "Out"
-        else:
+        if records["r"].nodes[0]._properties["name"] != records["p"]._properties["name"]:
             direction = "In"
+        else:
+            direction = "Out"
+        
+        for result in Argus:
+            if result["name"] == records["n"]._properties["name"]:
+                result["direction"] = ["In", "Out"]
+                flag = True
+                break
 
+        if flag: 
+            continue
+        
         nodes = []
         if (status == "PalabraEstimulo") or (status == "Estimulo"):
-            other_nodes = session.run("MATCH (n)-[r:ASOCIA_CON]-(p:Estimulo{name:$Estimulo}) RETURN n, r, p LIMIT 2",
-                                      Estimulo=name)
+            other_nodes = session.run("MATCH (n)-[r:ASOCIA_CON]-(p:Estimulo{name:$Estimulo}) RETURN n, r, p LIMIT 2", Estimulo=name)
             node1, node2 = other_nodes.values()
             nodes = [node1[0]._properties["name"], node2[0]._properties["name"]]
 
@@ -54,6 +65,57 @@ def generateJSON(session, result):
     return Argus
 
 
+#   Table
+def generateTableJSON(word):
+    # Create the driver
+    db_url = os.environ.get("GRAPHENEDB_BOLT_URL")
+    db_user = os.environ.get("GRAPHENEDB_BOLT_USER")
+    db_pass = os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
+    driver = GraphDatabase.driver(db_url, auth=(db_user, db_pass), encrypted=True)
+
+    # Create the session
+    session = driver.session()
+    result = session.run("MATCH (n)-[r:ASOCIA_CON]-(p:Estimulo{name:$Estimulo}) RETURN n, r, p", Estimulo = word)
+
+    # table:
+    # [ 
+    #   {
+    #       "name": word,
+    #       "direction": in/out,
+    #       "frecuency": float,
+    #       "time": float,
+    #       "association": float      
+    #   }
+    #   .
+    #   .
+    # ]
+
+    Argus = []
+    for records in result:
+        if(records["r"].nodes[0]._properties["name"] != "abeja"):
+            name = records["r"].nodes[0]._properties["name"]
+        else:
+            name = records["r"].nodes[1]._properties["name"]
+
+        if records["r"].nodes[0]._properties["name"] == records["p"]._properties["name"]:
+            direction = "Out"
+        else:
+            direction = "In"
+
+        frecuency = records["r"]._properties["frecuency"]
+        time = records["r"]._properties["time"]
+        association = records["r"]._properties["association"]
+        
+        temporalJSON = {"name": name, "direction": direction, "frecuency": frecuency, "time": time, "association": association}
+        Argus.append(temporalJSON)
+
+    session.close()
+    driver.close()
+
+    return json.dumps(Argus, ensure_ascii=False, indent=4)
+
+
+#   Search
 def allStimulus(word):
     # Create the driver
     db_url = os.environ.get("GRAPHENEDB_BOLT_URL")
@@ -71,5 +133,8 @@ def allStimulus(word):
         name = records[0]._properties['name']
         temporalJSON = {"name": name}
         Argus.append(temporalJSON)
+
+    session.close()
+    driver.close()
 
     return json.dumps(Argus, ensure_ascii=False, indent=4)
