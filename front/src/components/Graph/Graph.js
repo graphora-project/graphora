524a0b0e1e1b9ef1378f638200f92516714f684c
Alan Morales <alan_jesus99@hotmail.com>
2020-09-01 12:29:55 -0500

    added Graph component

---
+++ front/src/components/Graph/Graph.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import { GraphoraContext } from '../GraphoraContext'
+
+const Graph = () => {
+  const { wordRelations } = useContext(GraphoraContext)
+
+  return (
+    <>
+      <ul>
+        {wordRelations.map((relation) => (
+          <div key={relation.name}>
+            <h3>{relation.name}</h3>
+            <ul>
+              <li>status: {relation.status}</li>
+              <li>direction: {relation.direction}</li>
+            </ul>
+            <hr />
+          </div>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default Graph
