81d0e4ca7275a1c41709f57a03a145cb91a63974
Alan Morales <alan_jesus99@hotmail.com>
2020-09-29 10:49:29 -0500

    feat: added tertiaryNode

---
+++ front/src/graphSketch/nodes/tertiaryNode.js
@@ -0,0 +1,28 @@
+import Node from './node'
+
+export const TertiaryNode = ({
+  p5,
+  xCoordinate,
+  yCoordinate,
+  label,
+  relations,
+}) => {
+  const [baseNodeDraw] = Node({
+    p5,
+    xCoordinate,
+    yCoordinate,
+    label,
+    relations,
+  })
+  const color = '#FFBE67'
+  const ratio = 30
+
+  const draw = () => {
+    baseNodeDraw({ color, ratio })
+  }
+
+  return {
+    draw,
+    relations,
+  }
+}
