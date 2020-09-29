1f72c03ef4830d54d68cba5aebdf5900e176ae26
Alan Morales <alan_jesus99@hotmail.com>
2020-09-29 10:11:32 -0500

    test: testing clickableNode

--- front/src/graphSketch/graphSketch.js
+++ front/src/graphSketch/graphSketch.js
@@ -1,9 +1,28 @@
-export const graphSketch = (p) => {
-  p.setup = () => {
-    p.createCanvas(window.innerWidth, window.innerHeight)
+import { ClickeableNode } from './nodes'
+
+export const graphSketch = (p5) => {
+  const node1 = ClickeableNode({
+    p5,
+    xCoordinate: 100,
+    yCoordinate: 100,
+    label: 'estimulo',
+    onClick: () => alert('searching...'),
+  })
+
+  p5.setup = () => {
+    p5.createCanvas(
+      (window.innerWidth / 100) * 90,
+      (window.innerHeight / 100) * 90,
+    )
   }
 
-  p.draw = () => {
-    p.background(255)
+  p5.draw = () => {
+    p5.background(255)
+    p5.cursor('default')
+    node1.draw()
+  }
+
+  p5.mouseClicked = () => {
+    node1.mouseClicked()
   }
 }
