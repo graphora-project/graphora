3b6595656110fb30e2344e491801fdfcef6620c4
Alan Morales <alan_jesus99@hotmail.com>
2020-10-05 21:22:28 -0500

    removing unused code

--- front/src/components/organisms/Graph/Graph.js
+++ front/src/components/organisms/Graph/Graph.js
@@ -1,17 +1,13 @@
 import React, { useContext, useEffect, useRef } from 'react'
 import p5 from 'p5'
 import { GraphoraContext } from '../../GraphoraContext'
-import { WordCard } from '../../molecules/WordCard'
-import { LabelButton } from '../../atoms/Button'
 import { graphSketch } from '../../../graphSketch'
 
 const graph = graphSketch(p5)
 let p5Instance
 
 export const Graph = () => {
-  const { currentWord, relatedWords, goBackinHistory, searchWord } = useContext(
-    GraphoraContext,
-  )
+  const { currentWord, relatedWords, searchWord } = useContext(GraphoraContext)
   const ref = useRef()
   graph.setOnClickFunction((label) => {
     searchWord(label)
--- front/src/graphSketch/graph/graphBuilder.js
+++ front/src/graphSketch/graph/graphBuilder.js
@@ -1,15 +1,10 @@
-import dagre from 'dagre'
 import Graph from 'graphology'
-import { random, circlepack } from 'graphology-layout'
-import noverlap from 'graphology-layout-noverlap'
-import forceAtlas2 from 'graphology-layout-forceatlas2'
+import { random } from 'graphology-layout'
 import { ClickeableNode, RegularNode } from '../nodes'
-import { InArrow, OutArrow, InOutArrow } from '../arrows'
 import { P5Graph } from './graph'
 
-export const GraphBuilder = ({ p5, data, currentWord, onClickFunction }) => {
-  return fakeBuilder({ p5, data, currentWord, onClickFunction })
-}
+export const GraphBuilder = ({ p5, data, currentWord, onClickFunction }) =>
+  fakeBuilder({ p5, data, currentWord, onClickFunction })
 
 const fakeBuilder = ({ p5, data, currentWord, onClickFunction }) => {
   const graph = new Graph()
@@ -21,12 +16,6 @@ const fakeBuilder = ({ p5, data, currentWord, onClickFunction }) => {
   })
 
   const layout = random(graph, { scale: 500 })
-
-  /*
-  const layout = noverlap(graph, {
-    maxIterations: 500,
-  })
-  */
 
   const nodes = []
   const edges = []
@@ -62,52 +51,9 @@ const fakeBuilder = ({ p5, data, currentWord, onClickFunction }) => {
       )
     }
   })
-  /*
-  g.nodes().forEach((nodeName) => {
-    const node = g.node(nodeName)
 
-    if (node.isStimulate) {
-      nodes.push(
-        ClickeableNode({
-          p5,
-          xCoordinate: node.x,
-          yCoordinate: node.y,
-          label: nodeName,
-          onClick: onClickFunction,
-        }),
-      )
-    } else {
-      nodes.push(
-        RegularNode({
-          p5,
-          xCoordinate: node.x,
-          yCoordinate: node.y,
-          label: nodeName,
-        }),
-      )
-    }
-  })
-  */
-
-  /*
-  g.edges().forEach((edgeName) => {
-    const edge = g.edge(edgeName)
-    edges.push(
-      OutArrow({
-        p5,
-        initialX: edge.points[0].x,
-        initialY: edge.points[0].y,
-        finalX: edge.points[edge.points.length - 1].x,
-        finalY: edge.points[edge.points.length - 1].y,
-      }),
-    )
-  })
-
-  */
   return P5Graph({
     nodes,
     edges,
-    centerX: layout[currentWord].x,
-    centerY: layout[currentWord].y,
   })
 }
