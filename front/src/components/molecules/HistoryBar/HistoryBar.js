1d81a6ec98fc0312d46e0f28b7b4ed5db537110a
Alberto Polanco <prmus10@gmail.com>
2020-10-08 16:27:54 -0500

    test: trying to implement submenu

--- front/src/components/molecules/HistoryBar/HistoryBar.js
+++ front/src/components/molecules/HistoryBar/HistoryBar.js
@@ -1,25 +1,36 @@
 import React, {useContext} from 'react'
-import { Breadcrumbs, Link, Button } from '@material-ui/core'
+import { Breadcrumbs, Button } from '@material-ui/core'
 import { GraphoraContext } from '../../GraphoraContext'
+import { SubMenu } from '../SubMenu'
 
 export const HistoryBar = () => {
-  const { history, goBackinHistory } = useContext(GraphoraContext)
+  const { history, goBackInNHistory } = useContext(GraphoraContext)
+
   let historybar
   if (history.length < 5) {
-    historybar = (<Breadcrumbs>
-      {history.map((word, index) =>(
-          <Button onClick={() => {goBackinHistory(history.length - index)}} key={index}>{word}</Button>
-      ))}
-    </Breadcrumbs>)
+    historybar = <div style={{backgroundColor: "lightblue"}} >
+      <Breadcrumbs>
+        {history.map((word, index) =>(
+            <Button onClick={() => {goBackInNHistory(history.length - index)}} key={index} variant={"outlined"}>{word}</Button>
+        ))}
+      </Breadcrumbs>
+    </div>
   } else {
     const correctPosition = history.length - 3
     const subHistoryBar = []
+
     //[a,b,c,d,e,f,g,h,i,j] -> 10 - 3 = {7} [[a,b,c,d,e,f,g],h,i,j]
-    for (let i = 0; i <= correctPosition; i+=1) {
+
+    for (let i = 0; i < correctPosition; i+=1) {
       subHistoryBar.push(history[i])
     }
+
+    //<SubMenu subHistoryBar={subHistoryBar}/>
     historybar = (<Breadcrumbs>
 
+      {history.map((word, index) =>(
+          <Button onClick={() => {goBackInNHistory(history.length - index)}} key={index} variant={"outlined"}>{word}</Button>
+      ))}
     </Breadcrumbs>)
   }
 
