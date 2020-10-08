1b9dd5feeb10bb03a86823743b1c51c2fbacb18e
Alberto Polanco <prmus10@gmail.com>
2020-10-08 03:13:30 -0500

    test: Now you can return to a specific word

--- front/src/components/molecules/HistoryBar/HistoryBar.js
+++ front/src/components/molecules/HistoryBar/HistoryBar.js
@@ -1,31 +1,26 @@
-import React from 'react'
-import { Breadcrumbs, Link } from '@material-ui/core'
+import React, {useContext} from 'react'
+import { Breadcrumbs, Link, Button } from '@material-ui/core'
+import { GraphoraContext } from '../../GraphoraContext'
 
-export const HistoryBar = ({ history, action }) => {
-
-    const goBackinHistoryNTimes = () => {
-
-    }
-
-
+export const HistoryBar = () => {
+  const { history, goBackinHistory } = useContext(GraphoraContext)
   let historybar
   if (history.length < 5) {
-    historybar = (
-      <Breadcrumbs separator={'>'} style={{ color: 'black' }}>
-        {history.map((word) => (
-          <Link key={word}>{word}</Link>
-        ))}
-      </Breadcrumbs>
-    )
+    historybar = (<Breadcrumbs>
+      {history.map((word, index) =>(
+          <Button onClick={() => {goBackinHistory(history.length - index)}} key={index}>{word}</Button>
+      ))}
+    </Breadcrumbs>)
   } else {
-    let position = 0
-    const correctPosition = history.length - 4
-    history.map((word) => {
-      if (position < correctPosition) {
-        console.log(correctPosition)(<Breadcrumbs></Breadcrumbs>)
-      }
-      position++
-    })
+    const correctPosition = history.length - 3
+    const subHistoryBar = []
+    //[a,b,c,d,e,f,g,h,i,j] -> 10 - 3 = {7} [[a,b,c,d,e,f,g],h,i,j]
+    for (let i = 0; i <= correctPosition; i+=1) {
+      subHistoryBar.push(history[i])
+    }
+    historybar = (<Breadcrumbs>
+
+    </Breadcrumbs>)
   }
 
   return historybar
