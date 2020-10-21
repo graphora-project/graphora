53e3e23f09c610bde635610af6b649f5a20b5066
Alan Morales <alan_jesus99@hotmail.com>
2020-10-20 23:12:31 -0500

    fix: condition to create graph

--- front/src/graphSketch/graphSketch.js
+++ front/src/graphSketch/graphSketch.js
@@ -17,7 +17,7 @@ export const graphSketch = () => {
   const setData = (p5, _data, _currentWord) => {
     // as setState function in React is asynchronus, when a new word is searched
     // react sends the new currentWord but the data is the same as before, creating and error on the graph
-    if (data !== _data) {
+    if (data !== _data && _data.length > 1) {
       data = _data
       currentWord = _currentWord
       graph = GraphBuilder({ p5, data, currentWord, onClickFunction })
