f2d6826142d458e67467d294b925cd8abc796e01
Alan Morales <alan_jesus99@hotmail.com>
2020-10-27 21:47:45 -0600

    feat: using graphRepresentation

--- front/src/graphSketch/graph/graphBuilder.js
+++ front/src/graphSketch/graph/graphBuilder.js
@@ -1,170 +1,84 @@
-import Graph from 'graphology'
 import { StimulusNode, AssociateNode, MainNode } from '../nodes'
 import { P5Graph } from './graph'
 import { InArrow, OutArrow, InOutArrow } from '../arrows'
 import { CollisionsManager } from '../collisionsManager'
+import RandomLayout from '../layout/randomLayout'
 
-export const GraphBuilder = ({ data, currentWord, onClickFunction }) => {
-  const graph = new Graph()
-
-  graph.addNode(currentWord)
-  data.forEach((word) => {
-    if (!graph.nodes().includes(word.name)) {
-      graph.addNode(word.name)
-      graph.addEdge(currentWord, word.name)
-    }
-  })
-
-  const layout = layoutGraph(graph, { scale: 400 })
-
-  const nodes = generateNodes(data, currentWord, onClickFunction, layout)
-  const edges = generateEdges(data, nodes)
-
-  CollisionsManager().checkCollisions(nodes)
-
-  return P5Graph({
-    nodes,
-    edges,
-  })
-}
-
-const layoutGraph = (graph, { scale = 1 } = {}) => {
-  // quadrants with (0.5, 0.5) as its origin
-  const quadrants = [
-    {
-      x: {
-        min: 0,
-        max: 0.5,
-      },
-      y: {
-        min: 0,
-        max: 0.5,
-      },
-    },
-    {
-      x: {
-        min: 0.5,
-        max: 1,
-      },
-      y: {
-        min: 0,
-        max: 0.5,
-      },
-    },
-    {
-      x: {
-        min: 0.5,
-        max: 1,
-      },
-      y: {
-        min: 0.5,
-        max: 1,
-      },
-    },
-    {
-      x: {
-        min: 0,
-        max: 0.5,
-      },
-      y: {
-        min: 0.5,
-        max: 1,
-      },
-    },
-  ]
-  const nodes = graph.nodes()
-  const layout = {
-    [nodes[0]]: {
-      x: 0.5 * scale,
-      y: 0.5 * scale,
-    },
+export const GraphBuilder = () => {
+  const nodesTypes = {
+    main: MainNode,
+    estimulo: StimulusNode,
+    asociada: AssociateNode,
   }
 
-  // iterates the quadrants in a circular way. Uses random function to generate
-  // a random point within the current quadrant
-  let quadrantIndex = 0
-  for (let i = 1; i < nodes.length; i += 1) {
-    const quadrant = quadrants[quadrantIndex]
-    layout[nodes[i]] = {
-      x:
-        (Math.random() * (quadrant.x.max - quadrant.x.min) + quadrant.x.min) *
-        scale,
-      y:
-        (Math.random() * (quadrant.y.max - quadrant.y.min) + quadrant.y.min) *
-        scale,
-    }
-
-    quadrantIndex = (quadrantIndex + 1) % quadrants.length
+  const edgesTypes = {
+    in: InArrow,
+    out: OutArrow,
+    'in-out': InOutArrow,
   }
 
-  return layout
-}
+  const build = ({ graphRepresentation, onNodeClickedFunction }) => {
+    const layout = RandomLayout().generate(graphRepresentation.nodes, {
+      scale: 400,
+    })
+    const nodes = getNodesFromRepresentation(
+      graphRepresentation.nodes,
+      layout,
+      onNodeClickedFunction,
+    )
 
-const generateEdges = (data, nodes) => {
-  const edges = []
+    const edges = getEdgesFromRepresentation(graphRepresentation.edges, nodes)
 
-  for (let i = 0; i < data.length; i += 1) {
-    const node = data[i]
-    if (node.direction === 'in')
-      edges.push(
-        InArrow({
-          initialNode: nodes[0],
-          finalNode: nodes[i + 1],
-        }),
-      )
+    CollisionsManager().checkCollisions(nodes)
 
-    if (node.direction === 'out')
-      edges.push(
-        OutArrow({
-          initialNode: nodes[0],
-          finalNode: nodes[i + 1],
-        }),
-      )
-
-    if (node.direction === 'in-out')
-      edges.push(
-        InOutArrow({
-          initialNode: nodes[0],
-          finalNode: nodes[i + 1],
-        }),
-      )
+    return P5Graph({
+      nodes,
+      edges,
+    })
   }
 
-  return edges
-}
+  const getNodesFromRepresentation = (
+    nodesData,
+    layout,
+    onNodeClickedFunction,
+  ) => {
+    const nodes = []
 
-const generateNodes = (data, currentWord, onClickFunction, layout) => {
-  const nodes = []
+    nodesData.forEach((nodeData) => {
+      const newNodeConstructor = nodesTypes[nodeData.type]
+      const newNode = newNodeConstructor({
+        xCoordinate: layout[nodeData.name].x,
+        yCoordinate: layout[nodeData.name].y,
+        label: nodeData.name,
+      })
 
-  nodes.push(
-    MainNode({
-      xCoordinate: layout[currentWord].x,
-      yCoordinate: layout[currentWord].y,
-      label: currentWord,
-    }),
-  )
-  data.forEach((node) => {
-    if (node.status === 'estimulo') {
-      nodes.push(
-        StimulusNode({
-          xCoordinate: layout[node.name].x,
-          yCoordinate: layout[node.name].y,
-          label: node.name,
-          onClick: onClickFunction,
-        }),
-      )
-    }
+      if (newNode.isClickeable) {
+        newNode.setOnClickFunction(onNodeClickedFunction)
+      }
 
-    if (node.status === 'asociada') {
-      nodes.push(
-        AssociateNode({
-          xCoordinate: layout[node.name].x,
-          yCoordinate: layout[node.name].y,
-          label: node.name,
-        }),
-      )
-    }
-  })
+      nodes.push(newNode)
+    })
 
-  return nodes
+    return nodes
+  }
+
+  const getEdgesFromRepresentation = (edgesData, nodes) => {
+    const edges = []
+
+    edgesData.forEach((edgeData) => {
+      const newEdgeConstructor = edgesTypes[edgeData.type]
+      const newEdge = newEdgeConstructor({
+        initialNode: nodes[edgeData.initialNodeIndex],
+        finalNode: nodes[edgeData.finalNodeIndex],
+      })
+
+      edges.push(newEdge)
+    })
+
+    return edges
+  }
+
+  return {
+    build,
+  }
 }
