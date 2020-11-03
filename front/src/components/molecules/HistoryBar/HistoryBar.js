f1ed0d82a9e8fb6a7208ee117258a1da64a53eba
Alan Morales <alan_jesus99@hotmail.com>
2020-11-03 07:50:13 -0600

    feat: created useHistoryBar hook

--- front/src/components/molecules/HistoryBar/HistoryBar.js
+++ front/src/components/molecules/HistoryBar/HistoryBar.js
@@ -1,63 +1,8 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { GraphoraContext } from '../../GraphoraContext'
+import React from 'react'
 import { DropDown } from '../DropDown'
 import { Breadcrumb } from '../Breadcrumb'
 import { When } from '../../utils/When'
-
-const useHistoryBar = () => {
-  const { history, goBackInHistory } = useContext(GraphoraContext)
-  const [isCollapsed, setIsCollapsed] = useState(false)
-  const [collapsedData, setCollapsedData] = useState([])
-  const [unCollapsedData, setUnCollapsedData] = useState([])
-
-  useEffect(() => {
-    /*
-    if (history.length <= 3) {
-      const newUncollapsedData = history.map((word, index) => {
-        const timesToGoBack = history.length - (index + 1)
-        return {
-          label: word,
-          onClickFunction: () => goBackInHistory(timesToGoBack),
-        }
-      })
-      setUnCollapsedData(newUncollapsedData)
-      setCollapsedData([])
-    } else { */
-
-    const newUncollapsedData = []
-    const newCollapsedData = []
-    const indexToSlice = history.length - 2
-
-    for (let i = 0; i < history.length; i += 1) {
-      const word = history[i]
-      const timesToGoBack = history.length - (i + 1)
-
-      if (i < indexToSlice) {
-        newCollapsedData.push({
-          label: word,
-          onClickFunction: () => goBackInHistory(timesToGoBack),
-        })
-      } else {
-        newUncollapsedData.push({
-          label: word,
-          onClickFunction: () => goBackInHistory(timesToGoBack),
-        })
-      }
-    }
-    setCollapsedData(newCollapsedData)
-    setUnCollapsedData(newUncollapsedData)
-  }, [history])
-
-  useEffect(() => {
-    if (collapsedData.length > 0) {
-      setIsCollapsed(true)
-    } else {
-      setIsCollapsed(false)
-    }
-  }, [unCollapsedData, collapsedData])
-
-  return [isCollapsed, unCollapsedData, collapsedData]
-}
+import useHistoryBar from '../../../hooks/useHistoryBar'
 
 export const HistoryBar = () => {
   const [isCollapsed, unCollapsedData, collapsedData] = useHistoryBar()
---
+++ front/src/hooks/useHistoryBar.js
@@ -0,0 +1,46 @@
+import { useContext, useState, useEffect } from 'react'
+import { GraphoraContext } from '../components/GraphoraContext'
+
+const useHistoryBar = () => {
+  const { history, goBackInHistory } = useContext(GraphoraContext)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [collapsedData, setCollapsedData] = useState([])
+  const [unCollapsedData, setUnCollapsedData] = useState([])
+
+  useEffect(() => {
+    const newUncollapsedData = []
+    const newCollapsedData = []
+    const indexToSlice = history.length - 2
+
+    for (let i = 0; i < history.length; i += 1) {
+      const word = history[i]
+      const timesToGoBack = history.length - (i + 1)
+
+      if (i < indexToSlice) {
+        newCollapsedData.push({
+          label: word,
+          onClickFunction: () => goBackInHistory(timesToGoBack),
+        })
+      } else {
+        newUncollapsedData.push({
+          label: word,
+          onClickFunction: () => goBackInHistory(timesToGoBack),
+        })
+      }
+    }
+    setCollapsedData(newCollapsedData)
+    setUnCollapsedData(newUncollapsedData)
+  }, [history])
+
+  useEffect(() => {
+    if (collapsedData.length > 0) {
+      setIsCollapsed(true)
+    } else {
+      setIsCollapsed(false)
+    }
+  }, [unCollapsedData, collapsedData])
+
+  return [isCollapsed, unCollapsedData, collapsedData]
+}
+
+export default useHistoryBar
