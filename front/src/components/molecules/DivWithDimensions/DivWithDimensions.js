394625ca7d92b077dab030558c6dbbdfab5a5385
Alan Morales <alan_jesus99@hotmail.com>
2020-11-08 14:13:36 -0600

    feat: added DivWithDimensions component

---
+++ front/src/components/molecules/DivWithDimensions/DivWithDimensions.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { withSize } from 'react-sizeme'
+
+const Div = ({ size, onResize, children }) => {
+  useEffect(() => {
+    onResize(size)
+  }, [size.width, size.height])
+
+  return (
+    <div style={{ position: 'relative', height: '100%' }}>
+      <div style={{ position: 'absolute', width: '100%', height: size.height }}>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+Div.propTypes = {
+  size: PropTypes.object.isRequired,
+  onResize: PropTypes.func,
+  children: PropTypes.any,
+}
+
+export const DivWithDimensions = withSize({ monitorHeight: true })(Div)
